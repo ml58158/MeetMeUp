be8f201d5d6d14eec242e8e28dc47527c81417a5
Matt Larkin <mattlarkin@me.com>
2015-03-23 23:53:17 -0400

    First Commit
    Have JSON data parsing

--- MeetMeUp/ViewController.m
+++ MeetMeUp/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource>
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property NSArray *eventsArray;
+@property NSDictionary *resultsDict;
 
 @end
 
@@ -16,12 +19,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSURL *url = [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=477d1928246a4e162252547b766d3c6d"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+     {
+         self.resultsDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
+
+         self.eventsArray = [self.resultsDict objectForKey:@"results"];
+         [self.tableView reloadData];
+     }];
+    NSLog(@"Huzzah!!");
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+#pragma mark - UI TableView Datasource
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{ return self.eventsArray.count;
 }
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MeetUpID"];
+    NSDictionary *results = self.eventsArray[indexPath.row];
+    //NSDictionary *name = results[@"name"];
+    cell.textLabel.text = results[@"name"];
+    cell.detailTextLabel.text = results[@"address_1"];
+    return cell;
+}
+
 
 @end
+
+
+#pragma mark - helper methods
+
